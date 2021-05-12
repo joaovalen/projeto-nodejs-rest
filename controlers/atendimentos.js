47c3f278057247f459cb63f1a722b4582c5e4e6d
joaovalen <joaovpvalentini@gmail.com>
2021-05-12 08:12:18 -0300

    curso finalizado

--- controlers/atendimentos.js
+++ controlers/atendimentos.js
@@ -1,15 +1,41 @@
+const { restart } = require('nodemon')
+const atendimentos = require('../models/atendimentos')
 const Atendimento = require('../models/atendimentos')
 // Getting the Atendimento object from the models
 
 module.exports = app => {
-    app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos e está realizando um GET'))
+    app.get('/atendimentos', (req, res) => {
+        Atendimento.lista(res)
+    })
+    // Listening for GET
+
+    app.get('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+        // Turns user data into number
+
+        Atendimento.buscaPorId(id,res)
+    })
+    // Search using ID
 
     app.post('/atendimentos', (req, res) => {
         const atendimento = req.body
         // takes the data from the body
 
         Atendimento.adiciona(atendimento, res)
-        // Uses the data to add a new atendimento 
-      
+        // Uses the data to add a new atendimento  
+    })
+    // Listening for POST
+
+    app.patch('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+        const valores = req.body
+
+        Atendimento.altera(id,valores,res)
+    })
+
+    app.delete('/atendimentos/:id', (req,res) => {
+        const id = parseInt(req.params.id)
+        
+        Atendimento.deleta(id,res)
     })
 } 
--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,4 +1,5 @@
 const moment = require('moment')
+const { restart } = require('nodemon')
 // biblioteca para lidar com datas
 const conexao = require('../infraestrutura/conexao')
 
@@ -9,21 +10,106 @@ class Atendimento {
         // moment() gets the time and format() sets it to Year-Month-Day
         const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
         // takes the data field and its format inserted by the user and formats it to Year-Month-Day
-        const atendimentoDatado = {...atendimento, dataCriacao, data}
-
         
-        const sql = 'INSERT INTO Atendimentos SET ?'
+        const dataEhValida = moment(data).isSameOrAfter(dataCriacao)
+        // Checking if the assigned data is valid
+        const clienteEhValido = atendimento.cliente.length >= 5
+        // Checking if the client name is long enough 
+       
+        const validacoes = [
+            // Creates and array with two objects 
+            {
+                nome: 'data',
+                valido: dataEhValida,
+                // boolean
+                mensagem: 'Data deve ser maior ou igual a data atual'
+            },
+            {
+                nome: 'cliente',
+                valido: clienteEhValido,
+                // bolean
+                mensagem: 'Cliente deve ter pelo menos cinco caracteres'
+            }
+        ]
 
-        conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+        const erros = validacoes.filter(campo => !campo.valido)
+        // DUVIDA! Como essa parte funciona
+        const existemErros = erros.length 
+
+        if(existemErros) {
+            res.status(400).json(erros)
+        } else {
+            const atendimentoDatado = {...atendimento, dataCriacao, data}
+            // DUVIDAAAA Aqui criamos um array contendo as novas informações, pq os 3 pontos antes???
+
+            const sql = 'INSERT INTO Atendimentos SET ?'
+
+            conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+                if(erro) {
+                    res.status(400).json(erro)
+                    // error 400 stands for client side error, meaning the user did something wrong
+                } else {
+                    res.status(201).json(atendimento)
+                    // using httpstatuses.com we can inform better the error or success
+                }
+            })
+        }
+
+    }
+
+    lista(res) {
+        const sql = 'SELECT * FROM Atendimentos'
+
+        conexao.query(sql, (erro, resultados) => {
             if(erro) {
                 res.status(400).json(erro)
-                // error 400 stands for client side error, meaning the user did something wrong
             } else {
-                res.status(201).json(resultados)
-                // using httpstatuses.com we can inform better the error or success
+                res.status(200).json(resultados)
+            }
+        })
+    }
+
+    buscaPorId(id, res) {
+        const sql = 'SELECT * FROM Atendimentos WHERE id = ?'
+
+        conexao.query(sql, id, (erro, resultados) => {
+            const atendimento = resultados[0]
+            // Returning the object itself and not an array with only one object as resultado would return
+            if(erro) {
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json(atendimento)
+            }
+        })
+    }
+
+    altera(id, valores, res) {
+        if(valores.data){
+            valores.data = moment(valores.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
+        }
+
+        const sql = 'UPDATE Atendimentos SET ? WHERE id = ?'
+
+        conexao.query(sql, [valores, id], (erro, resultados) => {
+            // Returning the object itself and not an array with only one object as resultado would return
+            if(erro) {
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json({...valores,id})
+            }
+        })
+    }
+
+    deleta(id,res) {
+        const sql = 'DELETE FROM Atendimentos WHERE id = ?'
+
+        conexao.query(sql, id, (erro, resultados) => {
+            if(erro) {
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json({id})
             }
         })
     }
 }
-
 module.exports = new Atendimento 

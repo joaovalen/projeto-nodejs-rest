b736fd516b569fb7670cf9139f8bd946dbbf9159
joaovalen <joaovpvalentini@gmail.com>
2021-05-13 17:49:29 -0300

    pos mudanças gev

--- arquivos/uploadDeArquivos.js
+++ arquivos/uploadDeArquivos.js
@@ -1,11 +1,12 @@
 const fs = require('fs')
 
 module.exports = (originPath, nomeDoArquivo, callbackImagemCriada) => {
-    const finalPath = `.assets/imagens/${nomeDoArquivo}`
+    const finalPath = `./assets/${nomeDoArquivo}`
 
-    fs.createReadStream(originPath)
+    fs.createReadStream(`assets/${originPath}`)
         .pipe(fs.createWriteStream(finalPath))
         // Creates a write stream on the destination for the file
         .on('finish', () => callbackImagemCriada(finalPath))
         // Calls the callback function when the write stream is done
 }
+

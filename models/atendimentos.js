d0425a037be59bb92e6f5dea2bd9a289a7e41ec7
joaovalen <joaovpvalentini@gmail.com>
2021-05-19 10:27:25 -0300

    Validations working

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -8,9 +8,32 @@ const repositorio = require('../repositorios/atendimento.js')
 
 class Atendimento {
     constructor() {
-        this.dataEhValida = ({ data, dataCriacao }) =>
-            moment(data).isSameOrAfter(dataCriacao)
-        this.clienteEhValido = (tamanho) => tamanho >= 5
+        this.dataEhValida = ({ data, dataCriacao }) => {
+            console.log(data + " e a data criação: " +dataCriacao)
+            return moment(data).isSameOrAfter(dataCriacao,'year')
+        }
+            
+        // Basicamente aqui dentro você está recebendo um objeto
+        // Logo, o que você está comparando com >= 5 é um objeto, não um número
+        // Algo como { tamanho: 5 } >= 5
+        // O que sempre retorna false
+        // No caso, dentro desse objeto você tem a propriedade tamanho
+        // Você pode receber o objeto e pegar a propriedade tamanho dele assim
+        // (campo) => campo.tamanho >= 5
+        // Ou pegar direto como parâmetro o tamanho usando destructuring, assim
+        // ({ tamanho }) => tamanho >= 5
+        // FUNÇÃO ANTIGA this.clienteEhValido = (tamanho) => tamanho >= 5
+        // PS.: sugiro por ora criar as funções usando chaves, assim
+        this.clienteEhValido = campo => {
+        return campo.tamanho >= 5
+        }
+        // Porque aí fica mais fácil de debuggar, por exemplo
+        // this.clienteEhValido = campo => {
+        //   console.warn('campo', campo)
+        //   return campo.tamanho >= 5
+        // }
+        // Pra ver o que tem dentro do campo
+        // Só lembra que usando chaves, você tem que lembrar de dar return
 
         this.valida = parametros =>
             this.validacoes.filter(campo => {
@@ -34,7 +57,7 @@ class Atendimento {
         ]
     }
 
-    
+
     lista() {
         return repositorio.lista()
     }
@@ -78,13 +101,13 @@ class Atendimento {
                 cliente: {tamanho: valores.cliente.length}
             }
             const erros = this.valida(parametros)
-            const existemErros = erros.length 
-    
+            const existemErros = erros.length
+
             if(existemErros) {
                 return new Promise((reject) => reject(erros))
             } else {
                 const alteraçãoDatada = {...valores,dataCriacao,data}
-                
+
                 return repositorio.altera(id, alteraçãoDatada)
                     .then(resultados => {
                         return {...valores, id}
@@ -101,4 +124,4 @@ class Atendimento {
 
 }
 
-module.exports = new Atendimento 
+module.exports = new Atendimento
